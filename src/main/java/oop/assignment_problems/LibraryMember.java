95e2b827f0edcff678f7bb7bf63f93c06f213b5a
2503us <us2503@srmist.edu.in>
2026-08-29 01:59:41 +0530

    Fix F4 static and instance design

--- src/main/java/oop/assignment_problems/LibraryMember.java
+++ src/main/java/oop/assignment_problems/LibraryMember.java
@@ -2,24 +2,47 @@ package oop.assignment_problems;
 
 public class LibraryMember {
 
-    String name;
-    String memberId;
-    int booksIssued;
+    static String name;
+    static String memberId;
+    static int booksIssued;
+
+    String fixedName;
+    String fixedMemberId;
+    int fixedBooksIssued;
 
     static String libraryName = "Central Library";
     static int memberCount = 0;
 
+    /*
+     * name should not be static because each member has a different name.
+     * memberId should not be static because each member needs a different ID.
+     * booksIssued should not be static because each member can have a different number of books.
+     */
+
     LibraryMember(String name, int booksIssued) {
-        this.name = name;
-        this.booksIssued = booksIssued;
+        LibraryMember.name = name;
+        LibraryMember.booksIssued = booksIssued;
+        LibraryMember.memberId = "LM-" + booksIssued;
+    }
+
+    LibraryMember(String name, int booksIssued, boolean fixed) {
+        this.fixedName = name;
+        this.fixedBooksIssued = booksIssued;
 
         memberCount++;
+        this.fixedMemberId = "LM-" + (1000 + memberCount);
+    }
 
-        this.memberId = "LM-" + (1000 + memberCount);
+    void printBrokenMemberCard() {
+        System.out.println(
+                LibraryMember.name + " | " + LibraryMember.memberId
+        );
     }
 
     void printMemberCard() {
-        System.out.println(name + " | " + memberId);
+        System.out.println(
+                fixedName + " | " + fixedMemberId
+        );
     }
 
     static void printTotalMembers() {
@@ -28,11 +51,25 @@ public class LibraryMember {
 
     public static void main(String[] args) {
 
-        LibraryMember member1 =
+        LibraryMember broken1 =
                 new LibraryMember("Aditi", 2);
 
-        LibraryMember member2 =
+        LibraryMember broken2 =
                 new LibraryMember("Rohan", 3);
+
+        System.out.println("Broken version:");
+
+        broken1.printBrokenMemberCard();
+        broken2.printBrokenMemberCard();
+
+        LibraryMember member1 =
+                new LibraryMember("Aditi", 2, true);
+
+        LibraryMember member2 =
+                new LibraryMember("Rohan", 3, true);
+
+        System.out.println();
+        System.out.println("Fixed version:");
 
         member1.printMemberCard();
         member2.printMemberCard();
